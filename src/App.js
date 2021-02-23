64e787d3404049881917fac00820ea0c11afd8bb
Ojhal Patwa <patkarojhal26@gmail.com>
2021-02-23 15:02:11 +0530

    USE STATE

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import { render } from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: "Hello word!"
+    }
+
+  }
+  render() {
+    return (
+      <div>
+        <h3 style={{ color: "red" }}>{this.state.msg}</h3>
+      </div>
+    )
+  }
+
 }
+
+
 
 export default App;
