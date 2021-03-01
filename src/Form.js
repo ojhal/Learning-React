a89ecb03f97afedbb9f5cb59d0e00725641ae103
Ojhal Patwa <patkarojhal26@gmail.com>
2021-03-01 14:14:40 +0530

    use this.state

--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+  constructor(props) {
+   super(props);
+   this.state = {
+    msg: "Hi!,there"
+  }
+}
   render() {
     return (
       <div>
+       <p>{this.state.msg}</p>
         <h1>ojhal</h1>
       </div>
     )
