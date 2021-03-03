7c830d5b093faf8617651e00cee497a0367c0932
Ojhal Patwa <patkarojhal26@gmail.com>
2021-03-03 22:42:04 +0530

    use map method

--- src/List.js
+++ src/List.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class List extends Component {
+
   constructor(props) {
     super(props);
     this.state = {
@@ -17,10 +19,22 @@ class List extends Component {
     console.log('clicked')
   }
   render() {
+    const arr = [10, 20, 30, 40];
+    const newArr = arr.map(num => {
+      return <li>{num * 2}</li>
+    })
+    console.log('oldArray =', arr)
+    console.log('newArray =', newArr)
     return (
       <div>
         <h2>Message :</h2>
+
         <p>{this.state.msg}</p>
+        <ul className='a'>
+          {newArr}
+
+        </ul>
+
         <button onClick={this.handleClick}>
           Click here to know contents!
         </button>
