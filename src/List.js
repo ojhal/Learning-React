4fff38a8a896f3e2df4fdbfc9d6b188da9649cbb
Ojhal Patwa <patkarojhal26@gmail.com>
2021-03-04 13:24:23 +0530

    add functionality

--- src/List.js
+++ src/List.js
@@ -1,44 +1,75 @@
 import React, { Component } from 'react';
-import './App.css';
+// import './App.css';
 
 class List extends Component {
+  static defaultProps = {
+    userContent: ['React state', 'React Js', 'React Router']
+  }
 
   constructor(props) {
     super(props);
     this.state = {
-      msg: ["React Course", "react hooks"]
-
+      msg: 'React Course',
+      content: ''
     }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  usersArray() {
+    return (
+      <ul className='a'>
+        {this.props.userContent.map(content => (
+          <li>{content}</li>
+        ))}
+      </ul>
+    )
 
   }
 
-  handleClick = () => {
+
+  handleClick() {
     this.setState({
-      msg: "course"
+      msg: 'Course Content :',
+      content: this.usersArray()
+
     })
     console.log('clicked')
   }
   render() {
-    const arr = [10, 20, 30, 40];
-    const newArr = arr.map(num => {
-      return <li>{num * 2}</li>
-    })
-    console.log('oldArray =', arr)
-    console.log('newArray =', newArr)
+    // const arr = [10, 20, 30, 40];
+    // const newArr = arr.map(num => {
+    //   return <li>{num * 2}</li>
+    // })
+    // console.log('oldArray =', arr)
+    // console.log('newArray =', newArr)
     return (
       <div>
         <h2>Message :</h2>
+        <h3>{this.state.msg}</h3>
 
-        <p>{this.state.msg}</p>
-        <ul className='a'>
-          {newArr}
 
-        </ul>
 
-        <button onClick={this.handleClick}>
+        <p>{this.state.content}</p>
+
+
+        {/* <ul className='a'>
+          {arr.map(num => {
+            return <li>{num * 2}</li>
+          })}
+          {/* {newArr} */}
+
+        {/* </ul> * /} */}
+        {/* <ul className='a'>
+
+
+        </ul> */}
+
+
+        < button onClick={this.handleClick} >
           Click here to know contents!
-        </button>
-      </div>
+        </button >
+      </div >
     );
   }
 }
