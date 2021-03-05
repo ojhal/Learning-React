ffb7178b51145f06459a984f3058e4bf45bf4e4b
Ojhal Patwa <patkarojhal26@gmail.com>
2021-03-05 15:23:15 +0530

    add input tag

--- src/Form.js
+++ src/Form.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react'
 
-export default class File extends Component {
+export default class Form extends Component {
   render() {
     return (
-      <div>
-        <h1 className='form'>Fee Form</h1>
-        <p>Name:</p>
+      <div className='container1'>
+        <h3 className='form'>Application For Travel Document Fee</h3>
+        <p className='a'> First Name:</p>
         <input
           name="name"
           text="text"
-          placeholder="Enter name"
+          placeholder="First Name"
+        />
+
+        <p>Last Name:</p>
+        <input
+          name="name"
+          text="text"
+          placeholder="Last Name"
         />
 
         <p>Application No:</p>
@@ -19,12 +26,15 @@ export default class File extends Component {
           placeholder="Enter App No"
         />
 
-        <p>Father's Name:</p>
+        <p>Date Of Birth:</p>
         <input
           name="name"
           text="text"
-          placeholder="Enter Father's Name"
+          placeholder="DD/MM/YYYY"
         />
+        <br />
+
+        <button className='btn'>Submit</button>
 
 
 
