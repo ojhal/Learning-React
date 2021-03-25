0846e01542212cd58caaacfc84701353cff555e8
Ojhal Patwa <patkarojhal26@gmail.com>
2021-03-25 23:48:49 +0530

    using react-bootstrap

--- src/Card.js
+++ src/Card.js
@@ -1,52 +1,41 @@
-import React, { Component } from 'react'
+import React from 'react';
 
-export default class Card extends Component {
-  render() {
-    const App = () => {
-      return (
-        <div>
-          <h1> Welcome our new company </h1>
-          <div className="Container">
-            <div className="Row">
-              <div className="Col-sm"><Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-    </Card.Text>
-                  <button variant="primary">Go somewhere</button>
-                </Card.Body>
-              </Card></div>
-              <div className="Col-sm"><Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-    </Card.Text>
-                  <button variant="primary">Go somewhere</button>
-                </Card.Body>
-              </Card></div>
-              <div className="Col-sm"><Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="holder.js/100px180" />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-    </Card.Text>
-                  <button variant="primary">Go somewhere</button>
-                </Card.Body>
-              </Card></div>
 
+
+// render() {
+const Card = () => {
+  return (
+    <>
+      <h2 className="text-center text-danger text-capitalize my-5" > Welcome to our new company </h2>
+      <div className="container">
+        <div className="row">
+          <div className="col-sm"> <div class="card" />
+            <img class="card-img-top" src="https://picsum.photos/201/300" alt="..." height="200px" />
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
+          </div></div>
+        <div className="col-sm"> <div class="card" />
+          <img class="card-img-top" src="https://picsum.photos/200/302" alt="..." height="200px" />
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
-
+        </div></div>
+      <div className="col-sm"> <div class="card" />
+        <img class="card-img-top" src="https://picsum.photos/200/301" alt="..." height="200px" />
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
-      );
-    }
-  }
+      </div></>
 
+  );
+}
+
+export default Card;
+// }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 
 
 
