75ba5554b7b6ca75bf92b20bd6aa486b0276e2be
Ojhal Patwa <patkarojhal26@gmail.com>
2021-03-26 22:21:27 +0530

    add some changes

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ import React from 'react';
 // render() {
 const Card = () => {
   return (
-    <>
+    <React.Fragment>
       <h2 className="text-center text-danger text-capitalize my-5" > Welcome to our new company </h2>
       <div className="container">
         <div className="row">
@@ -32,7 +32,7 @@ const Card = () => {
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           <a href="#" class="btn btn-primary">Go somewhere</a>
         </div>
-      </div></>
+      </div></React.Fragment>
 
   );
 }
